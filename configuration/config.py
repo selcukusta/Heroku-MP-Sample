5b97b4f94be2baa0755975b6f13d22ad1fb6c0b1
Selçuk Usta <selcukusta@gmail.com>
2016-12-24 18:50:15 +0300

    Port is changed

--- configuration/config.py
+++ configuration/config.py
@@ -3,6 +3,6 @@ dirname = os.path.dirname(__file__)
 
 DEBUG = True
 COMPRESS_RESPONSE = True
-PORT = 5000
+PORT = int(os.environ.get("PORT", 5000))
 STATIC_PATH = os.path.join(dirname, '../assets')
 TEMPLATE_PATH = os.path.join(dirname, '../templates')
