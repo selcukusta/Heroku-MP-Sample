c1389da13e1fb3e177ce0cb02b7b72eb09da055e
Selçuk Usta <selcukusta@gmail.com>
2016-12-24 18:54:33 +0300

    Debug -> false

--- configuration/config.py
+++ configuration/config.py
@@ -1,7 +1,7 @@
 import os
 dirname = os.path.dirname(__file__)
 
-DEBUG = True
+DEBUG = False
 COMPRESS_RESPONSE = True
 PORT = int(os.environ.get("PORT", 5000))
 STATIC_PATH = os.path.join(dirname, '../assets')
